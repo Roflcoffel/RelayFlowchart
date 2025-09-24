664709abc96037d9380e0e16db77b1db485f5e38
Andreas Johansson <roflcoffel@gmail.com>
2025-09-24 22:08:41 +0200

    Flowchart will be created in p5js, only html was hell

--- flode.js
+++ flode.js
@@ -1,22 +1,22 @@
- var xoffset = 0, mouse_x = 0
+var xoffset = 0, mouse_x = 0
 var train = document.getElementById("train")
 train.onmousedown = MouseDown;
 
 function MouseDown(e) {
-e.preventDefault();
-mouse_x = e.clientX;
-document.onmouseup = Reset;
-document.onmousemove = Drag;
+    e.preventDefault();
+    mouse_x = e.clientX;
+    document.onmouseup = Reset;
+    document.onmousemove = Drag;
 }
 
 function Drag(e) {
-e.preventDefault();
-xoffset = mouse_x - e.clientX;
-mouse_x = e.clientX;
-train.style.left = (train.offsetLeft - xoffset) + "px";
+    e.preventDefault();
+    xoffset = mouse_x - e.clientX;
+    mouse_x = e.clientX;
+    train.style.left = (train.offsetLeft - xoffset) + "px";
 }
 
 function Reset() {
-document.onmouseup = null;
-document.onmousemove = null;
+    document.onmouseup = null;
+    document.onmousemove = null;
 }

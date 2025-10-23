aa1a92544bcdc49bd80560a1c702772f4ae7b9c3
Antares <roflcoffel@gmail.com>
2025-10-23 14:05:37 +0200

    Create a and b complexes

--- dynamic-index.js
+++ dynamic-index.js
@@ -12,14 +12,15 @@ async function fetchJSONData(file) {
 // We assume CD is the default complex
 async function Load(complex) {
     let comp = await fetchJSONData(complex)
-
+    let html = ""
+    
+    //Did the fetch work
     console.log(comp)
 
     let title = document.getElementById("title")
     let linje = document.getElementById("linje-img")
     let tbody = document.getElementById("table-body")
-
-    let html = ""
+    
     //Create a html string from the data
     for (let i = 0; i < comp["table-body"].length; i++) {
         const relay = comp["table-body"][i];
@@ -37,59 +38,10 @@ async function Load(complex) {
     let template = document.createElement("template")
     template.innerHTML = html
 
+    tbody.innerHTML = ""
     tbody.appendChild(template.content)
-    /* Recreate this using javsscript.
-        <tr>
-            <td><u>Iv</u></td>
-            <td>Spårledningsupptag</td>
-        </tr>
-        <tr>
-            <td><u>Sv</u></td>
-            <td>Spårledningsupptag</td>
-        </tr>
-        <tr>
-            <td><u>IIv</u></td>
-            <td>Spårledningsupptag</td>
-        </tr>
-        <tr>
-            <td><u>RVv</u></td>
-            <td>Manöverrelä för varningssignalering, repeteringsrelä för <u>Vv</u></td>
-        </tr>
-        <tr>
-            <td><u>Vv</u></td>
-            <td>Varningssignalering väg, startar signalering</td>
-        </tr>
-        <tr>
-            <td>Akv</td>
-            <td>Avkopplingsrelä för vägskyddsanläggning</td>
-        </tr>
-        <tr>
-            <td>TRAkvB</td>
-            <td>Tid Relä för kontroll om <u>Akv</u> är draget</td>
-        </tr>
-        <tr>
-            <td>TRVv</td>
-            <td>Tid Relä för återställning av <u>Vv</u> och varningssignalering vid en belägning av Iv eller IIv</td>
-        </tr>
-        <tr>
-            <td>FdIv</td>
-            <td>Färdriktningsrelä som styrs av Iv, för passageriktning Iv-Sv-<u>IIv</u></td>
-        </tr>
-        <tr>
-            <td>FdSv</td>
-            <td>Färdriktningsrelä som styrs av Sv, används för att avgöra passagekontrollen</td>
-        </tr>
-        <tr>
-            <td>FdIIv</td>
-            <td>Färdriktningsrelä som styrs av IIv, för passageriktning IIv-Sv-<u>Iv</u></td>
-        </tr>
-        <tr>
-            <td>Vsv</td>
-            <td>Kontrollrelä för aktiverad varningssignalering i Signaler mot banan</td>
-        </tr>
-
-
-    */
+    title.innerText = comp.title
+    linje.src = comp.linjesrc
 }
 
 Load("cd.json")
